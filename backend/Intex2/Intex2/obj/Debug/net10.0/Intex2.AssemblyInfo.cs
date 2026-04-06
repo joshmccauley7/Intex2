edbd7c57ceb066d2ec40dd26485d65c503aeb782
wlholland <will@hollandfam.com>
2026-04-06 10:59:00 -0600

    random crap

--- backend/Intex2/Intex2/obj/Debug/net10.0/Intex2.AssemblyInfo.cs
+++ backend/Intex2/Intex2/obj/Debug/net10.0/Intex2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b69501c56b7f867d1379a162e703a2388b60cee7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec6e60417db14ac587057d24cb69d96e90fdcb4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
