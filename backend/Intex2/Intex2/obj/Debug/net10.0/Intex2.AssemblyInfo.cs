5b356a0896989a83ca493746861a75add715ba1d
joshmccauley7 <joshmccauley7@gmail.com>
2026-04-06 17:03:56 -0600

    idk

--- backend/Intex2/Intex2/obj/Debug/net10.0/Intex2.AssemblyInfo.cs
+++ backend/Intex2/Intex2/obj/Debug/net10.0/Intex2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77eeaa5c04fe0d3b801080629e0737c3cf7e84ec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89cfd34dbc033394648e5723ff47373d9beaddbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Intex2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
