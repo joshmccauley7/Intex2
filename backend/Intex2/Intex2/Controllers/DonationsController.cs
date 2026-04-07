c85c13009eeecdb7746227574ccc6b13ac11c207
wlholland <will@hollandfam.com>
2026-04-07 14:10:33 -0600

    donation controller to enforce login

--- backend/Intex2/Intex2/Controllers/DonationsController.cs
+++ backend/Intex2/Intex2/Controllers/DonationsController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Stripe;
@@ -5,6 +6,7 @@ using Stripe.Checkout;
 
 [ApiController]
 [Route("api/donations")]
+[Authorize]
 public class DonationsController : ControllerBase
 {
     private readonly AppDbContext _db;
